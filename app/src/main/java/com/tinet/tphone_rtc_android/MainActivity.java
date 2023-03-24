ba475a9c380c03b95f8158933d3f687e8bd5d0e5
smile525 <zp045217@163.com>
2023-03-24 15:42:24 +0800

    初始提交

---
+++ app/src/main/java/com/tinet/tphone_rtc_android/MainActivity.java
@@ -0,0 +1,16 @@
+package com.tinet.tphone_rtc_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tinet.tphonertcandroid.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
